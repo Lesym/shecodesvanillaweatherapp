65c7264081d93fd9d311652691ec65227d54c0d1
Lesym <lesydiaroseline@gmail.com>
2024-10-22 17:46:40 +0200

    added search functionallity using API

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+function refreshweather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  //temperatureElement.innerHTML = response.data.temperature.current;
+  //console.log(response.data.temperature.current);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "511a91f0ccob731b14bda3bt51303cd4";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshweather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInputElement.value;
+  //let cityElement = document.querySelector("#weather-app-city");
+  //cityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
